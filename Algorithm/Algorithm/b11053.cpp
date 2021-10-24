8b633b8aaffe45bee020b0bb15b6c129b524cc3f
DongWook Kim <49301812+DongWookK@users.noreply.github.com>
2021-10-24 15:44:59 +0900

    LIS(b11053)
    가장 긴 수열

---
+++ Algorithm/Algorithm/b11053.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	
+	int n;
+	int a;
+	cin >> n;
+	vector<int>v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+
+	vector<int>s(n);//벡터 초기화파라미터? n개만큼 0으로
+
+	//0부터 순회하며
+	//이전것중에 가장 크고 카운트가 큰 애에서 +1
+	for (int i = 0; i < n; i++) {
+		s[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (v[j] < v[i] && s[i] < s[j]+1) {
+				s[i] = s[j] + 1;
+			}
+		}
+	}
+	int answer = 0;
+	for (int i = 0; i < n; i++) {
+		if (s[i] > answer) {
+			answer = s[i];
+		}
+	}
+	cout << answer;
+	return 0;
+}
