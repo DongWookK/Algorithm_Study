b2cc9526017bac19337c12a3acad0533aeaa07e4
DongWook <dev.Dongwook@gmail.com>
2021-11-29 18:51:06 +0900

    Prim Algorithm ~Add Node

--- Algorithm/Algorithm/Prim_Algorithm.cpp
+++ Algorithm/Algorithm/Prim_Algorithm.cpp
@@ -39,3 +39,47 @@ void push(PriorityQueue* pq, Edge* edge) {
 	}
 	pq->count++;
 }
+
+Edge* pop(PriorityQueue* pq) {
+	if (pq->count <= 0) return NULL;
+	Edge* res = pq->heap[0];
+	pq->count--;
+	pq->heap[0] = pq->heap[pq->count];
+	int now = 0, leftChild = 1, rightChild = 2;
+	int target = now;
+	while (leftChild < pq->count) {
+		if (pq->heap[target]->cost > pq->heap[leftChild]->cost) target = leftChild;
+		if (pq->heap[target]->cost > pq->heap[rightChild]->cost) target = rightChild;
+		if (target == now) break; //가장 작을때
+		else {
+			swap(pq->heap[now], pq->heap[target]);
+			now = target;
+			leftChild = now * 2 + 1;
+			rightChild = now * 2 + 1;
+		}
+	}
+	return res;
+}
+
+
+struct Node {
+	Edge* data;
+	struct Node* next;
+};
+
+Node** adj;
+int d[NODE_MAX];
+
+void addNode(Node** target, int index, Edge* edge) {
+	if (target[index] == NULL) { //특정 정점에 연결된 간선이 하나도 없다면
+		target[index] = new Node;
+		target[index]->data = edge;
+		target[index]->next = NULL;
+	}
+	else {
+		Node* node = new Node;
+		node->data = edge;
+		node->next = target[index];
+		target[index] = node;
+	}
+}
