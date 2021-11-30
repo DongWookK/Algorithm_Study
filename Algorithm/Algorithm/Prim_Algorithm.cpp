84c74c871205f3cf8394de53b871390a04da38d6
DongWook <dev.Dongwook@gmail.com>
2021-11-30 20:58:32 +0900

    Prim Main (Need bug fix)

--- Algorithm/Algorithm/Prim_Algorithm.cpp
+++ Algorithm/Algorithm/Prim_Algorithm.cpp
@@ -67,9 +67,6 @@ struct Node {
 	struct Node* next;
 };
 
-Node** adj;
-int d[NODE_MAX];
-
 void addNode(Node** target, int index, Edge* edge) {
 	if (target[index] == NULL) { //특정 정점에 연결된 간선이 하나도 없다면
 		target[index] = new Node;
@@ -82,4 +79,63 @@ void addNode(Node** target, int index, Edge* edge) {
 		node->next = target[index];
 		target[index] = node;
 	}
+}
+
+Node** adj;
+int d[NODE_MAX]; //정점의 방문여부 확인
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	adj = new Node*[(n + 1)]; //double pointer(노드배열)에 대한 동적할당
+	for (int i = 1; i <= n; i++) {
+		adj[i] = NULL;
+	}
+	PriorityQueue* pq;
+	//프림알고리즘 내에 사용할 우선순위 큐
+	pq = new PriorityQueue;
+	pq->count = 0;
+
+	for (int i = 0; i < m; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		Edge* edge1 = new Edge;
+		edge1->node = b;
+		edge1->cost = c;
+		addNode(adj, a, edge1);
+		Edge* edge2 = new Edge;
+		edge2->node = a;
+		edge2->cost = c;
+		addNode(adj, b, edge2);
+	}
+
+	//프림 알고리즘
+	long long res = 0;
+	Edge* start = new Edge;
+	start->cost = 0; start->node = 1; push(pq, start);
+	//우선순위큐에서 정점(Node)의 개수만큼 반복
+	for (int i = 1; i <= n; i++) {
+		int nextNode = -1, nextCost = INT_MAX;
+		while (1) { //방문하지 않은 노드중에서 비용이 제일 적은 노드를 우선순위큐에서 꺼내줌
+			Edge* now = pop(pq);
+			if (now == NULL)break;
+			nextNode = now->node;
+			if (!d[nextNode]) { //d가 전역변수이므로 기본적으로 0, 방문했다면 1로 되어있음.
+				nextCost = now->cost; break;
+			}
+		}
+		if (nextCost == INT_MAX) cout << "연결 그래프가 아닙니다.\n";
+
+		res += nextCost;
+		cout << nextCost << endl;
+		d[nextCost] = (int)1; //여기랑 여기 밑에 부분 이해 필요
+		Node* cur = adj[nextNode];
+
+		while (cur != NULL) {
+			push(pq, cur->data);
+			cur = cur->next; 
+		} //그 노드와 인접한 모든 노드를 확인하면서 정보를 우선순위큐에 넣어둠
+	}
+	cout << res << endl;
+	return 0;
 }
