ec6cc1f07408cd77c91058bf42ebbc38820043c2
DongWook <dev.Dongwook@gmail.com>
2023-02-19 21:02:00 +0900

    remove 2022 solved problem

--- SolvedProblem/2022/b2667.cpp
+++ SolvedProblem/2022/b2667.cpp
@@ -1,33 +1,41 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS 
 #include <iostream>
-#include <stdio.h>
-#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+
+/*-----------------
+
+단지번호붙이기 b2667
+
+------------------*/
 int a[25][25];
-bool check[25][25];
-int ans[30] = { 0, };
-int cnt = 1;
+int d[25][25]; //방문했는지 체크
+int ans[25 * 25];
+int n;
+int dx[4] = { 0,0,1,-1 };
+int dy[4] = { 1,-1,0,0 };
 
-void dfs(int x, int y) {
-	check[x][y] = true;
-
-
-	if (a[x][y - 1] = 1 && check[x][y - 1] == false)
-		dfs(x, y - 1);
-	if (a[x][y + 1] = 1 && check[x][y + 1] == false)
-		dfs(x, y + 1);
-	if (a[x - 1][y] = 1 && check[x - 1][y] == false)
-		dfs(x - 1, y);
-	if (a[x + 1][y] = 1 && check[x + 1][y] == false)
-		dfs(x + 1, y);
+void dfs(int x, int y, int num)
+{
+	d[x][y] = num;
+	for (int k = 0; k < 4; k++)
+	{
+		int nx = x + dx[k];
+		int ny = y + dy[k];
+		if (nx >= 0 && ny >= 0 && nx < n && ny < n)
+		{
+			if (a[nx][ny] == 1 && d[nx][ny] == 0)
+				dfs(nx, ny, num);
+		}
+	}
 
 }
 
-int main() {
-
-	int n;
+int main()
+{
+	//정사각형 크기 입력
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
@@ -35,21 +43,34 @@ int main() {
 		}
 	}
 
-	cout << endl;
-	cout << endl;
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (a[i][j] == 1 || a[i][j] == 0)
-			{
-				cnt = 1;
-				dfs(i, j);
-			}
+	//dfs 실행
+	int cnt = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			//조건 설립 - 방문하지않았고, 건물이 있으면
+			if (a[i][j] == 1 && d[i][j] == 0)
+				dfs(i, j, ++cnt);
 				
 		}
-		cout << endl;
 	}
-	
+
+
+	printf("%d\n", cnt);
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			ans[d[i][j]] += 1;
+		}
+	}
+	sort(ans + 1, ans + cnt + 1);
+
+	for (int i = 1; i <= cnt; i++) {
+		printf("%d\n", ans[i]);
+	}
 
 	return 0;
 }
